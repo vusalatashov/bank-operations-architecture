a02ed15a875e04d5a3f32c831574d8ec32320484
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2025-02-07 17:40:56 +0400

    ms-photo-storage

---
+++ ms-photo-storage/src/main/java/az/xalqbank/msphotostorage/MsPhotoStorageApplication.java
@@ -0,0 +1,13 @@
+package az.xalqbank.msphotostorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPhotoStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPhotoStorageApplication.class, args);
+    }
+
+}
